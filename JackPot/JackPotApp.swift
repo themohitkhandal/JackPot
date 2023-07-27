dcfb2b54ac62f46ba9f76381f9daea3b413541a4
Mohit Khandal <mohit.khandal@anduriltechnologies.com>
2023-07-27 16:05:47 +0530

    Initial Commit

---
+++ JackPot/JackPotApp.swift
@@ -0,0 +1,17 @@
+//
+//  JackPotApp.swift
+//  JackPot
+//
+//  Created by Anurag Bajpai on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct JackPotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
